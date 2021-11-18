31308990bf5cfbe44a3326cd8dd649b22090d94f
kosstommani <kosstommani@naver.com>
2021-11-18 13:42:17 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe('Default Test Set', () => {
+    it('test1 should be passed', () => {
+        console.log('test1 passed')
+    });
+
+    it('test2 should be passed', () => {
+        console.log('test2 passed')
+    });
+});
